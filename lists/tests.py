831a5019dc72387793d178016efd653651e1f3c6
brsg <@gmail.com>
2016-05-18 12:51:29 -0400

    Refactor home page view to use template, changed test

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home_page
 
@@ -11,13 +12,10 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  # Create httprequest object that django will see when a browser asks for a page
-        response = home_page(request) # Pass request to home page view; Httpresponse class whose properties we can call
-        self.assertTrue(response.content.startswith(b'<html>'))
-        # This checks that the page starts with an html tag. Note that the data type for response.content is raw bytes
-        self.assertIn(b'<title>To-Do Lists</title>', response.content)
-        # This checks for the bytestring for an html title label with the title 'To-Do Lists'
-        self.assertTrue(response.content.endswith(b'</html>'))
-        # Same as the first check, but searches for the closed tag at the end of the page
+        request = HttpRequest()  # Create http request object
+        response = home_page(request)  # Pass request to homepage view
+        expected_html = render_to_string('home.html') # Load template home.html and convert it to html string
+        self.assertEqual(response.content.decode(), expected_html)  # Compare expect html string to content on web page
 
+       
 
