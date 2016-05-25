42cd147739a2a71ad47c86c2ba3e3e1831cccd9e
brsg <bobby.grado@gmail.com>
2016-05-25 15:50:08 -0400

    updated functional tests to test for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -42,13 +42,43 @@ class NewVisitorTest(LiveServerTestCase):  # organize test into class based on T
         # When he hits enter, the page should update and list-
         # "1: Buy bananas" as an item on a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')  # verifies that the url contains /lists/
+        self.check_for_row_in_list_table('1:Buy bananas')  # Verifies that '1:Buy bananas' is in some id_list_table
 
         inputbox.send_keys('Eat bananas')
         inputbox.send_keys(Keys.ENTER)
 
         self.check_for_row_in_list_table('1: Buy bananas')
         self.check_for_row_in_list_table('2: Eat bananas')
+
+
+        # Now let a new user, Francis open up the site. We don't want him to see the previous user's list
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # First, check that Edith's list is nowhere to be seen
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy bananas', page_text)
+        self.assertNotIn('Eat bananas', page_text)
+
+        # Let Francis create his own new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Check that Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')  # verifies that the url contains /lists/
+        self.assertNotEqual(francis_list_url, edith_list_url)  # verifies that the 2 lists do not have identical URLs
+
+        # Check that Franics', and only Francis', list is here
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy bananas', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Did we pass the test? If so, fail us!
         self.fail('finish the test')
 
 if __name__ == '__main__':
