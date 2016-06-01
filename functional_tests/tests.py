bb1cf3496cdbed49a478c13e1f35f70c4e59baa4
brsg <bobby.grado@gmail.com>
2016-06-01 15:51:33 -0400

    added bootstrap, refactored html templates with css

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,86 +1,114 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
+class NewVisitorTest(StaticLiveServerTestCase):
 
-class NewVisitorTest(LiveServerTestCase):  # organize test into class based on TestCase subclass
-
-    def setUp(self):  # 'setup' method that opens the browser and creates object for it
+    def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):  # 'shutdown' method that closes the browser
+    def tearDown(self):
         self.browser.refresh()
         self.browser.quit()
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        #print(rows, type(rows))
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it(self):  # Methods that begin with 'test' are run by the test runner
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get(self.live_server_url)
 
-        # open homepage of the website
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-
-        # The title of the webpage should include 'To-Do'
-        header_text = self.browser.find_element_by_tag_name('h1').text  # Selenium method for retrieving header name
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # Website prompts user to enter a to-do item right away
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
         )
 
-        # User tries to enter 'Buy bananas;
-        inputbox.send_keys('Buy bananas')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-        # When he hits enter, the page should update and list-
-        # "1: Buy bananas" as an item on a to-do list table
+        # When she hits enter, she is taken to a new URL,
+        # and now the page lists "1: Buy peacock feathers" as an item in a
+        # to-do list table
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')  # verifies that the url contains /lists/
-        self.check_for_row_in_list_table('1: Buy bananas')  # Verifies that '1:Buy bananas' is in some id_list_table
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        inputbox.send_keys('Eat bananas')
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('1: Buy bananas')
-        self.check_for_row_in_list_table('2: Eat bananas')
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+        # Now a new user, Francis, comes along to the site.
 
-        # Now let a new user, Francis open up the site. We don't want him to see the previous user's list
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        # First, check that Edith's list is nowhere to be seen
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy bananas', page_text)
-        self.assertNotIn('Eat bananas', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-        # Let Francis create his own new list
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
-        # Check that Francis gets his own unique URL
+        # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/.+')  # verifies that the url contains /lists/
-        self.assertNotEqual(francis_list_url, edith_list_url)  # verifies that the 2 lists do not have identical URLs
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # Check that Franics', and only Francis', list is here
-        page_text = self.browser.find_element_by_tag_name('body')
-        self.assertNotIn('Buy bananas', page_text)
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        # Did we pass the test? If so, fail us!
-        self.fail('finish the test')
+        # Satisfied, they both go back to sleep
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices that the input box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
